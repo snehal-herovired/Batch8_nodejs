46d15c862b0ac9b9f533589b14082ffe8686f91a
Snehal <snehal9140@gmail.com>
2023-04-02 17:04:31 +0530

    bbatch 8

---
+++ index.js
@@ -0,0 +1,38 @@
+// module fs
+// filesystem
+
+
+const fs = require("fs");
+
+
+//parallel execution
+// 123456789
+//serial execution
+console.log("start");
+fs.readFile("./num1.txt", "utf-8", (err, data) => {
+    console.log("we are inside callback function", data);
+    fs.readFile("./num2.txt", "utf-8", (err, data) => {
+        console.log("we are inside callback function", data);
+        fs.readFile("./num3.txt", "utf-8", (err, data) => {
+            console.log("we are inside callback function", data);
+            
+            // some activity based on the data of read file
+        })
+        // some activity based on the data of read file
+    })
+    // some activity based on the data of read file
+})
+
+
+// let data = fs.readFileSync("./test.txt", "utf-8");
+// console.log(data);
+
+console.log("end");
+
+//start
+// output of filereading
+// end
+
+// start
+// end
+//
