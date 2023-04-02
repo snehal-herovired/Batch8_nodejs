cc4908b8383d8e757236c1108eaf268cb18510fc
Swatantra chaurasia <chaurasiyaswatantra65@gmail.com>
2023-04-02 18:56:40 +0530

    dir name .path

--- filesystem/index.js
+++ filesystem/index.js
@@ -17,7 +17,29 @@ let folderPath = __dirname;
 fs.readdir(folderPath, "utf-8", (err, data)=>{
     console.log(data);
     //output of reading folder
+    // let arrayoffiles = data;
+    // let readingdata = fs.readFileSync(arrayoffiles[2], "utf-8", )
+    // console.log(readingdata, "reading the data of file");
+
+
     let arrayoffiles = data;
-    let readingdata = fs.readFileSync(arrayoffiles[2], "utf-8", )
-    console.log(readingdata, "reading the data of file");
+    for (let index = 0; index < arrayoffiles.length; index++) {
+        let isfile = fs.lstatSync(arrayoffiles[index]).isFile();
+        if (isfile) {
+            let data = fs.readFileSync(arrayoffiles[index], "utf-8");
+            console.log(data,"------------file reading----------");
+        }
+        let isfolder = fs.lstatSync(arrayoffiles[index]).isDirectory();
+        if (isfolder) {
+            //
+            let direcotrypath_of_this_file = path.dirname(isfolder);
+            console.log(direcotrypath_of_this_file);
+        }
+        
+    }
+    let filename = data[2];
+    let direcotrypath_of_this_file = path.extname(filename);
+    console.log(direcotrypath_of_this_file);
+
 })
+
