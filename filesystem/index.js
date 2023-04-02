925e05575b9a825ad4229986219cc9429874c918
Swatantra chaurasia <chaurasiyaswatantra65@gmail.com>
2023-04-02 18:14:32 +0530

    File system read/write file

--- filesystem/index.js
+++ filesystem/index.js
@@ -1,11 +1,23 @@
 const fs = require ("fs")
 
-fs.writeFile("test.txt", "HeroVired", (err)=>{
-    console.log(err)
-})
+// fs.writeFile("test.txt", "HeroVired", (err)=>{
+//     console.log(err)
+// })
 
-fs.writeFileSync("sample.txt", "heroviredsync")
+// fs.writeFileSync("sample.txt", "heroviredsync")
 
-fs.appendFile("test.txt", "I m learning FSD", (err)=>{
-    console.log(err)
+// fs.appendFile("test.txt", "I m learning FSD", (err)=>{
+//     console.log(err)
+// })
+
+
+// reading folder
+
+let folderPath = __dirname;
+fs.readdir(folderPath, "utf-8", (err, data)=>{
+    console.log(data);
+    //output of reading folder
+    let arrayoffiles = data;
+    let readingdata = fs.readFileSync(arrayoffiles[2], "utf-8", )
+    console.log(readingdata, "reading the data of file");
 })
