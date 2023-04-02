0ec489aa088f2e2541aabf33b5bab8e2c0df323e
Swatantra chaurasia <chaurasiyaswatantra65@gmail.com>
2023-04-02 19:53:26 +0530

    input in node

---
+++ filesystem/nodeinput.js
@@ -0,0 +1,10 @@
+let name1 = process.argv[2]
+let name2  = process.argv[3]//this number is the argument from terminal
+
+// console.log(name1)
+
+
+// import the add fun
+
+const add = require("./example.js")
+console.log(add(3,6))
---
+++ filesystem/sample.js
@@ -0,0 +1,6 @@
+function add (a,b){
+    c= a+b
+    return c
+}
+
+module.exports = add
