595351718e8dc0dcbf20b59057993169af3a1ae0
lord-vector <r.anuj.agrawal@gmail.com>
2023-04-02 18:34:58 +0530

    added index file inside my name folder to check the code

---
+++ Anuj/index.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+
+//appending a file
+// fs.appendFile("writeMyFirstAsync.txt", "Appending R", (err) => {
+//   console.log(err);
+// });
+//writing a file
+// fs.writeFile("writeMyFirstAsync.txt", "Anuj Agrawal", (err) => {
+//   console.log(err);
+//   fs.writeFile("writeMyFirstAsyncTest", "Anuj Agrawal", (err) => {
+//     console.log(err);
+//   });
+// });
+
+// fs.writeFileSync("writeMyFirstFileSync.txt", "Anuj is Tetsing", (err) => {
+//   console.log(err);
+// });
+
+//appending a file
+// fs.appendFile("writeMyFirstAsync.txt", "Appending R", (err) => {
+//   console.log(err);
+// });
+
+// fs.readdir(__dirname, "utf-8", (err, data) => {
+//   console.log(data);
+//   data.forEach((ele, index) => {
+//     if (ele.includes(".txt")) {
+//       fs.readFile(ele, "utf-8", (err, dataIn) => {
+//         console.log(dataIn);
+//       });
+//     }
+//   });
+// });
+
+fs.readdir(__dirname, "utf-8", (err, data) => {
+  console.log(data);
+  let directoryData = [];
+  data.forEach((ele, index) => {
+    let isDirectory = fs.lstatSync(ele).isDirectory();
+    let isFile = fs.lstatSync(ele).isFile();
+    if (isFile && ele.includes(".txt")) {
+      fs.readFile(ele, "utf-8", (err, dataIn) => {
+        console.log(dataIn);
+      });
+    }
+    if (isDirectory) {
+      directoryData.push(ele);
+    }
+  });
+  console.log("Directories - ", directoryData);
+});
