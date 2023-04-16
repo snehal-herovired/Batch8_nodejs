6fdd41efc2f14631b7e0ca5d00f5d4c9a6eb516d
Snehal <snehal9140@gmail.com>
2023-04-16 18:52:44 +0530

    mongodb methods

--- mongodb/index.js
+++ mongodb/index.js
@@ -24,29 +24,72 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     username: {
         type: String,
-        required:true
+        required: true
     },
     email: {
         type: String,
-        required:true
+        required: true
     },
     password: {
         type: String,
-        required:true
+        required: true
     }
-  });
+});
 
 const UserModel = mongoose.model('UserDatabase', UserSchema);
-  // this model will be used to intercat with our db using our apis.
+// this model will be used to intercat with our db using our apis.
 
-app.post("/register", (req, res) => {
+app.post("/register", async(req, res) => {
     const { username, email, password } = req.body
-    console.log(username,email,password);
-    res.json({
-        message:"data recieved"
+    console.log(username, email, password);
+    let userobj = {
+        username: username,
+        password: password,
+        email: email
+    }
+    try {
+        let data = await UserModel({email}).save();
+    if (data) {
+        return res.json({
+            message:"data inserted succesfuylly"
+        })
+    }
+    return res.json({
+        message:"data not inserted"
     })
+    } catch (error) {
+        return res.json({
+            message:"some error"
+        })
+    }
+    // method to store the data in mongodb;
+        
+        // error: multiple responses :req header can not be set after it has been sent to the client;
+    
+    
+
 })
 
+app.post("/user", async(req, res) => {
+    const { email } = req.body;
+    // to find data based on email address provided';
+
+   try {
+       const data = await UserModel.find({ email });
+       if (data) {
+           return res.json({
+               message:"data exists"
+           })
+       }
+       return res.json({
+           message:"data does not exists"
+       })
+   } catch (error) {
+       return res.json({
+        message:"error in finding the data"
+    })
+   }
+})
 
 app.listen(5000, () => {
     console.log("server running");
