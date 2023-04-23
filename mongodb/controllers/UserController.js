42dca068c72e136752d2737b07f10df89576f2e5
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2023-04-23 18:28:55 +0530

    Adding UserController and UserRouter files

---
+++ mongodb/controllers/UserController.js
@@ -0,0 +1,54 @@
+const UserModel = require("../models/db.schema");
+
+const registerController = async(req, res) => {
+    const { username, email, password } = req.body
+    console.log(username, email, password);
+    let userobj = {
+        username: username,
+        password: password,
+        email: email
+    }
+    try {
+        let data = await UserModel({email}).save();
+    if (data) {
+        return res.json({
+            message:"data inserted succesfuylly"
+        })
+    }
+    return res.json({
+        message:"data not inserted"
+    })
+    } catch (error) {
+        return res.json({
+            message:"some error"
+        })
+    }
+    // method to store the data in mongodb;
+    // error: multiple responses :req header can not be set after it has been sent to the client;
+}
+
+const userController = async(req, res) => {
+    const { email } = req.body;
+    // to find data based on email address provided';
+
+   try {
+       const data = await UserModel.find({ email });
+       if (data) {
+           return res.json({
+               message:"data exists"
+           })
+       }
+       return res.json({
+           message:"data does not exists"
+       })
+   } catch (error) {
+       return res.json({
+        message:"error in finding the data"
+    })
+   }
+}
+
+module.exports = {
+    registerController,
+    userController
+}
--- mongodb/index.js
+++ mongodb/index.js
@@ -40,57 +40,53 @@ mongoose.connect(process.env.MONGO_URL)
 // const UserModel = mongoose.model('UserDatabase', UserSchema);
 // this model will be used to intercat with our db using our apis.
 
-app.post("/register", async(req, res) => {
-    const { username, email, password } = req.body
-    console.log(username, email, password);
-    let userobj = {
-        username: username,
-        password: password,
-        email: email
-    }
-    try {
-        let data = await UserModel({email}).save();
-    if (data) {
-        return res.json({
-            message:"data inserted succesfuylly"
-        })
-    }
-    return res.json({
-        message:"data not inserted"
-    })
-    } catch (error) {
-        return res.json({
-            message:"some error"
-        })
-    }
-    // method to store the data in mongodb;
-        
-        // error: multiple responses :req header can not be set after it has been sent to the client;
-    
-    
+// app.post("/register", async(req, res) => {
+//     const { username, email, password } = req.body
+//     console.log(username, email, password);
+//     let userobj = {
+//         username: username,
+//         password: password,
+//         email: email
+//     }
+//     try {
+//         let data = await UserModel({email}).save();
+//     if (data) {
+//         return res.json({
+//             message:"data inserted succesfuylly"
+//         })
+//     }
+//     return res.json({
+//         message:"data not inserted"
+//     })
+//     } catch (error) {
+//         return res.json({
+//             message:"some error"
+//         })
+//     }
+//     // method to store the data in mongodb;
+//     // error: multiple responses :req header can not be set after it has been sent to the client;
+// })
 
-})
+// app.post("/user", async(req, res) => {
+//     const { email } = req.body;
+//     // to find data based on email address provided';
 
-app.post("/user", async(req, res) => {
-    const { email } = req.body;
-    // to find data based on email address provided';
-
-   try {
-       const data = await UserModel.find({ email });
-       if (data) {
-           return res.json({
-               message:"data exists"
-           })
-       }
-       return res.json({
-           message:"data does not exists"
-       })
-   } catch (error) {
-       return res.json({
-        message:"error in finding the data"
-    })
-   }
-})
+//    try {
+//        const data = await UserModel.find({ email });
+//        if (data) {
+//            return res.json({
+//                message:"data exists"
+//            })
+//        }
+//        return res.json({
+//            message:"data does not exists"
+//        })
+//    } catch (error) {
+//        return res.json({
+//         message:"error in finding the data"
+//     })
+//    }
+// })
 
 app.listen(port, () => {
     console.log("server running");
